16704f7dd2a860f80182cd874848145bb823f14b
florian-buehler@outlook.com <Cal!sthen!c5>
2021-01-25 12:30:03 +0100

    chore: Add database connection, basic model and seeding.

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-﻿const express = require('express');
+﻿const express = require('express')
 const path = require('path')
+const mongoose = require('mongoose')
+const Campground =require('./models/campground')
 
-const app = express();
+mongoose.connect('mongodb://localhost:27017/camprDb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"))
+db.once("open", () => {
+  console.log("Database connected")
+})
+
+const app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
@@ -9,8 +23,12 @@ app.set('views', path.join(__dirname, 'views'))
 app.get('/', (req, res) => {
   res.render('home')
 })
+app.get('/makecampground', async (req, res) => {
+  const camp = new Campground({title: 'My Backyard', description: 'camping'})
+  await camp.save()
+  res.send(camp)
+})
 
 app.listen(3000, () => {
   console.log('Serving on port 3000')
 })
-
