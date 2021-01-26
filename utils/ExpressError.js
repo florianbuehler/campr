a1ece81900c0f570b4865cfef53d0150e77fb9e3
florian-buehler@outlook.com <Cal!sthen!c5>
2021-01-26 14:04:00 +0100

    chore: Add basic server and client side error handling.

---
+++ utils/ExpressError.js
@@ -0,0 +1,9 @@
+﻿class ExpressError extends Error {
+  constructor(message, statusCode) {
+    super()
+    this.message = message
+    this.statusCode = statusCode
+  }
+}
+
+module.exports = ExpressError
---
+++ utils/catchAsync.js
@@ -0,0 +1,5 @@
+﻿module.exports = (func) => {
+  return (req, res, next) => {
+    func(req, res, next).catch(next)
+  }
+}
