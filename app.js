75469c6420ac608016825fe8e1a4cbf843ee2fe5
florian-buehler@outlook.com <Cal!sthen!c5>
2021-01-26 15:37:55 +0100

    chore: Add Joi for schema validation.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const path = require('path')
 const mongoose = require('mongoose')
 const ejsMate = require('ejs-mate')
+const { campgroundSchema } = require('./schemas')
 const catchAsync = require('./utils/catchAsync')
 const ExpressError = require('./utils/ExpressError')
 const methodOverride = require('method-override')
@@ -28,6 +29,16 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body)
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(',')
+    throw new ExpressError(msg, 400)
+  }
+
+  next()
+}
+
 app.get('/', (req, res) => {
   res.render('home')
 })
@@ -42,11 +53,8 @@ app.get(
 
 app.post(
   '/campgrounds',
+  validateCampground,
   catchAsync(async (req, res, next) => {
-    if (!req.body.campground) {
-      throw new ExpressError('Invalid Campground Data', 400)
-    }
-
     const campground = new Campground(req.body.campground)
     await campground.save()
     res.redirect(`/campgrounds/${campground._id}`)
@@ -67,6 +75,7 @@ app.get(
 
 app.put(
   '/campgrounds/:id',
+  validateCampground,
   catchAsync(async (req, res) => {
     const { id } = req.params
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
