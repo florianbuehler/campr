bda9d77a3bf78443b6b25d38372e2b64666f6637
florian-buehler@outlook.com <Cal!sthen!c5>
2021-01-26 14:19:40 +0100

    chore: Add basic error template.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path')
 const mongoose = require('mongoose')
 const ejsMate = require('ejs-mate')
 const catchAsync = require('./utils/catchAsync')
+const ExpressError = require('./utils/ExpressError')
 const methodOverride = require('method-override')
 const Campground = require('./models/campground')
 
@@ -42,6 +43,10 @@ app.get(
 app.post(
   '/campgrounds',
   catchAsync(async (req, res, next) => {
+    if (!req.body.campground) {
+      throw new ExpressError('Invalid Campground Data', 400)
+    }
+
     const campground = new Campground(req.body.campground)
     await campground.save()
     res.redirect(`/campgrounds/${campground._id}`)
@@ -86,8 +91,18 @@ app.get(
   })
 )
 
+app.all('*', (req, res, next) => {
+  next(new ExpressError('Page Not Found', 404))
+})
+
 app.use((err, req, res, next) => {
-  res.send('bug')
+  const { statusCode = 500 } = err
+
+  if (!err.message) {
+    err.message = 'Something went wrong!'
+  }
+
+  res.status(statusCode).render('error', { err })
 })
 
 app.listen(3000, () => {
