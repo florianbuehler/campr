9fddc12781b8d7dbddb13a5c49402e23d207d463
florian-buehler@outlook.com <Cal!sthen!c5>
2021-01-30 15:23:12 +0100

    feat: Add redirect to the page visited before login prompt.

--- routes/users.js
+++ routes/users.js
@@ -35,13 +35,16 @@ router.get('/login', (req, res) => {
 
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
   req.flash('success', 'Welcome back!')
-  res.redirect('/campgrounds')
+  const redirectUrl = req.session.returnTo || '/campgrounds'
+  delete req.session.returnTo
+
+  res.redirect(redirectUrl)
 })
 
 router.get('/logout', (req, res) => {
   req.logout()
-  res.flash('success', 'Goodbye!')
-  res.redirect('/campgrounds')
+  req.flash('success', 'Goodbye!')
+  res.redirect('/login')
 })
 
 module.exports = router
