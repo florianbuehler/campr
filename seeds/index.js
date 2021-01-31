98c456ed27971a5086f4a38e7e70071e769ca6dd
florian-buehler@outlook.com <Cal!sthen!c5>
2021-01-31 18:36:15 +0100

    feat: Add cluster map.

--- seeds/index.js
+++ seeds/index.js
@@ -27,7 +27,7 @@ const seedDB = async () => {
   const user = new User({ username: 'testuser', email: 'testuser@test.invalid' })
   const registeredUser = await User.register(user, 'password')
 
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 300; i++) {
     const random1000 = Math.floor(Math.random() * 1000)
     const price = Math.floor(Math.random() * 20) + 10
 
@@ -40,7 +40,7 @@ const seedDB = async () => {
       price: price,
       geometry: {
         type: 'Point',
-        coordinates: [-113.1331, 47.0202]
+        coordinates: [cities[random1000].longitude, cities[random1000].latitude]
       },
       images: [
         {
