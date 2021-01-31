5f9e7b75cd8ff9198d1dfe5656a2d0368f212593
florian-buehler@outlook.com <Cal!sthen!c5>
2021-01-31 10:02:22 +0100

    feat: Add edit image functionality.

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -35,14 +35,10 @@ module.exports.showCampground = async (req, res) => {
 
 module.exports.updateCampground = async (req, res) => {
   const { id } = req.params
-  const campground = await Campground.findById(id)
-
-  if (!campground) {
-    req.flash('error', 'Cannot find that campground!')
-    return res.redirect('/campgrounds')
-  }
-
-  await Campground.findByIdAndUpdate(id, { ...req.body.campground })
+  const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
+  const images = req.files.map((f) => ({ url: f.path, filename: f.filename }))
+  campground.images.push(...images)
+  await campground.save()
 
   req.flash('success', 'Successfully updated the campground!')
   res.redirect(`/campgrounds/${campground._id}`)
