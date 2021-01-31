f9bc98120818f33bf067df95d665fd311624cbc3
florian-buehler@outlook.com <Cal!sthen!c5>
2021-01-31 09:52:24 +0100

    chore: Fix seed data due to image upload.

--- seeds/index.js
+++ seeds/index.js
@@ -35,10 +35,19 @@ const seedDB = async () => {
       author: registeredUser._id,
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-      image: 'https://source.unsplash.com/collection/483251',
       description:
         'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab ad adipisci deleniti dicta eos est facilis ipsam iure laborum magni minima modi, neque obcaecati odio odit possimus, provident quas qui quia quos repellendus reprehenderit sunt totam velit veniam vitae voluptas? Consectetur dolorem eveniet id illo, minima placeat sint voluptatum!',
-      price: price
+      price: price,
+      images: [
+        {
+          url: 'https://res.cloudinary.com/do2d93jxt/image/upload/v1612082057/campr/qmpdbzxy9fehgk6a03bf.jpg',
+          filename: 'campr/qmpdbzxy9fehgk6a03bf'
+        },
+        {
+          url: 'https://res.cloudinary.com/do2d93jxt/image/upload/v1612082057/campr/qgotpsqjcpcus42xgzba.jpg',
+          filename: 'campr/qgotpsqjcpcus42xgzba'
+        }
+      ]
     })
     await camp.save()
   }
